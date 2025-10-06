f1e2f45676b10e911fe681e9b3094cd10d5bcb6d
PQH tobi <phanquochung2511@gmail.com>
2025-10-06 14:36:04 +0700

    sub menu

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
 import Header from "./Header";
 import React from "react";
@@ -9,7 +9,33 @@ const Layouts = ({ children }) => {
         <Layout>
             <Header />
             <Content className="container_layout">
-                {children}
+                <div className="container_content">
+                    <div className="subMenu">
+                        <Button type="primary">
+                            <span>&#128202;</span>
+                            <span style={{ color: "#fff", fontWeight: 500 }}>
+                                Primary button
+                            </span>
+                        </Button>
+
+                        <Button style={{ border: "none" }}>
+                            <span>&#128221;</span>
+                            <span style={{ color: "#43536a", fontWeight: 500 }} >
+                                File New Claim
+                            </span>
+                        </Button>
+
+                        <Button style={{ border: "none" }}>
+                            <span>&#128269;</span>
+                            <span style={{ color: "#43536a", fontWeight: 500 }}>
+                                Track Claims
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="block_content">
+                        {children}
+                    </div>
+                </div>
             </Content>
         </Layout>
     )
