e9ab5fb01b9a10d40aea2073c9308fa771ccf9f0
PQH tobi <phanquochung2511@gmail.com>
2025-10-30 09:01:20 +0700

    homePage + MainAlbum

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import './homeStyle.scss'
-import Card_cus from '../../components/common/card/Card_cus';
+import './homeStyle.scss';
 import ram from '../../assets/images/dev/ram.jpg'
 import Submenu from '../../components/common/submenu/Submenu';
 import CardExtra from '../../components/common/cardExtra/CardExtra';
+import { TrashIcon, FileDirectorySymlinkIcon } from "@primer/octicons-react";
+import { Button, Image } from 'antd';
 const Home = () => {
 
     const [dataCard, setDataCard] = useState([
@@ -45,16 +46,38 @@ const Home = () => {
     const [dataCardExtra, setDataCardExtra] = useState({
         title: 'Extra Card Title',
         desc: 'This is a description for the extra card.',
-        children: [
-            <div style={{ backgroundColor: '#f0f0f0', padding: '10px', borderRadius: '5px' }}>Child Component 1</div>,
-            <div style={{ backgroundColor: '#e0e0e0', padding: '10px', borderRadius: '5px' }}>Child Component 2</div>
-        ]
     })
     return (
         <>
             <Submenu dataSubmenu={dataSubmenu} />
             <CardExtra dataCardExtra={dataCardExtra} />
-            <Card_cus dataCard={dataCard} />
+            <div className='container_homePage'>
+                <div className='container_daily'>
+                    <div className='box'>
+                        <span>Lịch</span>
+                        <br />
+                        <span>Thêm ngày, loại ngày, vòng lặp</span>
+                        <span></span>
+                    </div>
+                    <div className='box'>
+                        <span>Mỗi ngày</span>
+                        <br />
+                        <span>Số ngày trước đó</span>
+                        <br />
+                        <span>Level</span>
+                        <br />
+                        <span>Số ngày hoàn thành</span>
+                        <br />
+                        <span>Kiểm tra</span>
+                    </div>
+                    <div className='box'>
+                        <span>Chỉ ghi trong ngày</span>
+                    </div>
+                </div>
+                <div className='container_workList'>
+                    
+                </div>
+            </div>
         </>
     )
 }
